e746cfd6cfc7926a754e54e02245a4206479c090
drogosenturi <violoncello82@gmail.com>
2024-03-21 14:28:36 -0500

    last run took 2 hrs for 7 files with break points 50 by 2's. testing different parallelization and less breaks next.

--- rscripts/plot_mantel.r
+++ rscripts/plot_mantel.r
@@ -1,7 +1,20 @@
 setwd('~/soraida_r/mantel_analysis')
 library(vegan)
+library(ggplot2)
+library(gridExtra)
 results <- readRDS('nursery_files/test_result2.rds')
-print(results[[1]])
+print(results[[7]])
+## plot multiple into pdf
+plots <- function(i) {
+    output <- plot(results[[i]])
+    return(
+        output
+    )
+}
+pdf("plots/test_plots_2", onefile = TRUE)
+lapply(1:7, plots)
+dev.off()
+
 plot(results[[1]])
 jpeg("test_plot.jpg")
 results[[5]] |> plot()
