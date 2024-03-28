58031b2c7df073ec6b5e9ae22ecb9fc598e51bbb
drogosenturi <violoncello82@gmail.com>
2024-03-28 15:37:38 -0500

    plot mantel updated to extract all results into one vector

--- rscripts/plot_mantel.r
+++ rscripts/plot_mantel.r
@@ -1,10 +1,12 @@
-setwd('~/soraida_r/mantel_analysis')
+setwd('~/soraida_r/')
 library(vegan) ## you must load vegan to read the results properly!
 library(ggplot2)
 library(gridExtra) ## to grid the plots
+library(parallel)
+library(gtools)
 
 # load in results to list 'results'
-results <- readRDS('nursery_files/test_result_1-8.rds') 
+results <- readRDS('local_results/*') 
 print(results[[7]]) # check that it's correct
 
 # make it into a dataframe if you want to manipulate it
@@ -13,6 +15,36 @@ print(df)
 
 # otherwise, you can just extract that first result with $mantel.res
 results[[1]]$mantel.res
+
+# read in all files into one object
+files <- paste0('~/soraida_r/local_results/',
+                    list.files('~/soraida_r/local_results/',
+                    recursive = TRUE))
+files <- mixedsort(files)
+read_in <- function(i) {
+    return(
+        readRDS(files[i])
+    )
+}
+extracted <- lapply(1:length(files), read_in)
+
+# while loop to extract all 500 results
+i <- 1
+while(i < 39) {
+    new <- append(new, extracted[[i]][1:length(extracted[[i]])])
+    i <- i + 1
+}
+new <- new[-1] #remove first object creation line
+saveRDS(new, "full_results.rds")
+
+# extract only the mantel results
+mantel_vals <- function(i) {
+    return (
+        new[[i]]$mantel.res
+    )
+}
+result_only <- lapply(1:length(new), mantel_vals)
+saveRDS(result_only, "mantel_results_only.rds")
 
 ## plot multiple into pdf
 plots <- function(i) {
