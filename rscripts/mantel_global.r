1e2944477dc39b1c7bc4a236f1796df066f2222b
drogosenturi <violoncello82@gmail.com>
2024-03-29 11:14:04 -0500

    successfully got results for 1-40

--- rscripts/mantel_global.r
+++ rscripts/mantel_global.r
@@ -14,6 +14,7 @@ mantel_global <- function(i) {
     return(
         mantel(species_dists[[i]], patch_dists)
     )
+    gc()
 }
 
 # load in patch dists
@@ -26,23 +27,25 @@ message("start 1 ", Sys.time())
 species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_1-40.rds")
 
 # apply function
-result <- mclapply(1:40, mantel_global, mc.cores = 40)
+result <- mclapply(1:40, mantel_global, mc.cores = 15)
 
 # save result
 saveRDS(result, "~/mantel_files/global_results/global_1-40.rds")
-Sys.sleep(1)
+Sys.sleep(30)
 message("finished 1 ", Sys.time())
 
 ## RUN 2
 message("start 2 ", Sys.time())
+rm(list = c('species_dists', 'result')
+gc()
 
 # load in species_dists
 species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_41-80.rds")
 
 # apply function
-result <- mclapply(1:40, mantel_global, mc.cores = 40)
+result <- mclapply(1:40, mantel_global, mc.cores = 15)
 
 # save result
 saveRDS(result, "~/mantel_files/global_results/global_41-80.rds")
-Sys.sleep(1)
+Sys.sleep(30)
 message("finished 2 ", Sys.time())
