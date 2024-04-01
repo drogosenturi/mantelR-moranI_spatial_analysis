47967fdfbc32c53d9b0e23c3b2149cd0095c33a4
drogosenturi <violoncello82@gmail.com>
2024-04-01 12:41:27 -0500

    froze after one run again, attempting new

--- rscripts/mantel_global.r
+++ rscripts/mantel_global.r
@@ -20,6 +20,7 @@ mantel_global <- function(i) {
 
 # load in patch dists
 patch_dists <- readRDS("~/mantel_files/patch_dists.rds")
+Sys.sleep(10)
 
 ## RUN 1
 #message("start 1 ", Sys.time())
@@ -84,20 +85,20 @@ patch_dists <- readRDS("~/mantel_files/patch_dists.rds")
 #message("finished 4 ", Sys.time())
 
 ## RUN 5
-message("start 5 ", Sys.time())
-rm(list = c('species_dists', 'result'))
-gc()
+#message("start 5 ", Sys.time())
+#rm(list = c('species_dists', 'result'))
+#gc()
 
 # load in species_dists
-species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_161-200.rds")
+#species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_161-200.rds")
 
 # apply function
-result <- mclapply(1:40, mantel_global, mc.cores = 10)
+#result <- mclapply(1:40, mantel_global, mc.cores = 10)
 
 # save result
-saveRDS(result, "~/mantel_files/global_results/global_161-200.rds")
-Sys.sleep(10)
-message("finished 5 ", Sys.time())
+#saveRDS(result, "~/mantel_files/global_results/global_161-200.rds")
+#Sys.sleep(60)
+#message("finished 5 ", Sys.time())
 
 ## RUN 6
 message("start 6 ", Sys.time())
@@ -106,6 +107,7 @@ gc()
 
 # load in species_dists
 species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_201-240.rds")
+Sys.sleep(10)
 
 # apply function
 result <- mclapply(1:30, mantel_global, mc.cores = 15)
@@ -113,7 +115,7 @@ append(result, mclapply(31:40, mantel_global, mc.cores = 10))
 
 # save result
 saveRDS(result, "~/mantel_files/global_results/global_201-240.rds")
-Sys.sleep(10)
+Sys.sleep(60)
 message("finished 6 ", Sys.time())
 
 ## RUN 7
@@ -123,6 +125,7 @@ gc()
 
 # load in species_dists
 species_dists <- readRDS("~/mantel_files/sp_dist_mimicry_241-280.rds")
+Sys.sleep(10)
 
 # apply function
 result <- mclapply(1:40, mantel_global, mc.cores = 15)
