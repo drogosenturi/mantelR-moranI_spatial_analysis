2556f626f6d19ed64c444a0e2e9ff61315d77625
drogosenturi <violoncello82@gmail.com>
2024-04-10 17:50:47 -0500

    fixed patch sorting so that all patches are in same order, and thus species dist matrices line up with patch_dist matrix

--- rscripts/mantel_load_species.r
+++ rscripts/mantel_load_species.r
@@ -1,7 +1,7 @@
 ##              BEST LOCAL MANTEL WITH XY COORDS             ##
 message("start ", Sys.time())
 setwd("~/mantel_files/mimicry_runs/")
-#setwd("~/soraida_r/mantel_analysis/nursery_files/")
+#setwd("~/soraida_r/mantel_analysis/nursery_files/") #home desktop
 ## Create function to load all packages
 loadPackages <- function(packages) {
     lapply(c("data.table", "parallel", "ecodist",
@@ -21,10 +21,16 @@ file_path <- mixedsort(file_path, decreasing=TRUE)
 # create a df containing all patch coordinates, make headers same
 # and remove richness and block columns
 df_patches <- fread("~/mantel_lab/nursery_files/PatchRichnessEnd-101.csv")
+#df_patches <- fread("PatchRichnessEnd-101.csv") #for home desktop
 colnames(df_patches)[1] <- "P.xcor"
 colnames(df_patches)[2] <- "P.ycor"
-df_patches <- df_patches[,-c(3:5)]
+colnames(df_patches)[3] <- "patch.ID"
+df_patches <- df_patches[,-c(4:5)]
 head(df_patches)
+
+# sort the patch list
+df_patches <- df_patches[order(df_patches$patch.ID),]
+head(df_patches[3])
 
 # list of DF with each item being full dataframe
 load_df <- function(z) {
@@ -70,9 +76,21 @@ fill_in_coords <- function(i) {
         joined
     )
 }
-dffin <- mclapply(1:500, fill_in_coords, mc.cores = 40)
+dfcoords <- mclapply(1:500, fill_in_coords, mc.cores = 40)
 #length(as.data.frame(dffin[[7]])[,1]) # check length
 rm(dfSO)
+
+# sort the final dataframe
+sort_dfcoords <- function(i) {
+    df_temp <- as.data.frame(dfcoords[[i]])
+    df_temp <- df_temp[order(df_temp$patch.ID),]
+    return(
+        df_temp
+    )
+}
+dffin <- mclapply(1:500, sort_dfcoords, mc.cores = 40)
+head(dffin[[1]][3])
+head(dffin[[100]][3])
 
 # clean up the garbage
 gc()
