3d40aac3b30aac7f0784dde5f8477b74c369180f
drogosenturi <violoncello82@gmail.com>
2024-05-13 17:48:07 -0500

    changed mc.preschedule to false

--- morans_i/morans_local.r
+++ morans_i/morans_local.r
@@ -34,7 +34,7 @@ local_moran <- function(i) {
 
 ## RUN 1
 message("local test start 1: ", Sys.time())
-result <- mclapply(1:120, local_moran, mc.cores = 40)
+result <- mclapply(1:120, local_moran, mc.preschedule = FALSE, mc.cores = 40)
 message("local test finish 1: ", Sys.time())
 
 # save as RDS
@@ -42,42 +42,42 @@ saveRDS(result, "~/moran_files/local_results/mimicry_results_1-120.rds")
 message("file 1 saved", Sys.time())
 gc()
 
-## RUN 2
-message("local test start 2: ", Sys.time())
-result <- mclapply(121:240, local_moran, mc.cores = 40)
-message("local test finish 2: ", Sys.time())
+# ## RUN 2
+# message("local test start 2: ", Sys.time())
+# result <- mclapply(121:240, local_moran, mc.cores = 40)
+# message("local test finish 2: ", Sys.time())
 
-# save as RDS
-saveRDS(result, "~/moran_files/local_results/mimicry_results_121-240.rds")
-message("file 2 saved", Sys.time())
-gc()
+# # save as RDS
+# saveRDS(result, "~/moran_files/local_results/mimicry_results_121-240.rds")
+# message("file 2 saved", Sys.time())
+# gc()
 
-## RUN 3
-message("local test start 3: ", Sys.time())
-result <- mclapply(241:360, local_moran, mc.preschedule = FALSE, mc.cores = 40)
-message("local test finish 3: ", Sys.time())
+# ## RUN 3
+# message("local test start 3: ", Sys.time())
+# result <- mclapply(241:360, local_moran, mc.preschedule = FALSE, mc.cores = 40)
+# message("local test finish 3: ", Sys.time())
 
-# save as RDS
-saveRDS(result, "~/moran_files/local_results/mimicry_results_241-360.rds")
-message("file 3 saved", Sys.time())
-gc()
+# # save as RDS
+# saveRDS(result, "~/moran_files/local_results/mimicry_results_241-360.rds")
+# message("file 3 saved", Sys.time())
+# gc()
 
-## RUN 4
-message("local test start 4: ", Sys.time())
-result <- mclapply(361:480, local_moran, mc.cores = 40)
-message("local test finish 4: ", Sys.time())
+# ## RUN 4
+# message("local test start 4: ", Sys.time())
+# result <- mclapply(361:480, local_moran, mc.cores = 40)
+# message("local test finish 4: ", Sys.time())
 
-# save as RDS
-saveRDS(result, "~/moran_files/local_results/mimicry_results_361-480.rds")
-message("file 4 saved", Sys.time())
-gc()
+# # save as RDS
+# saveRDS(result, "~/moran_files/local_results/mimicry_results_361-480.rds")
+# message("file 4 saved", Sys.time())
+# gc()
 
-## RUN 5
-message("local test start 5: ", Sys.time())
-result <- mclapply(481:500, local_moran, mc.cores = 40)
-message("local test finish 5: ", Sys.time())
+# ## RUN 5
+# message("local test start 5: ", Sys.time())
+# result <- mclapply(481:500, local_moran, mc.cores = 40)
+# message("local test finish 5: ", Sys.time())
 
-# save as RDS
-saveRDS(result, "~/moran_files/local_results/mimicry_results_481-500.rds")
-message("file 5 saved", Sys.time())
-q()
+# # save as RDS
+# saveRDS(result, "~/moran_files/local_results/mimicry_results_481-500.rds")
+# message("file 5 saved", Sys.time())
+# q()
