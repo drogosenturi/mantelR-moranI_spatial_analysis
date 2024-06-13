64c2f49e53b9f66ca9a7b2938e1075efe1e0b543
drogosenturi <violoncello82@gmail.com>
2024-06-13 10:58:41 -0500

    new scripts for single files

---
+++ mantel_scripts/mantel_local_single.r
@@ -0,0 +1,55 @@
+##              Mantel local for control experiment            ##
+setwd("~/mantel_lab/nursery_files/")
+#setwd("~/soraida_r/mantel_analysis/nursery_files/")
+## Create function to load all packages
+loadPackages <- function(packages) {
+    lapply(c("data.table", "parallel",
+             "tidyr", "dplyr", "vegan"),
+             require, character.only = TRUE)
+}
+loadPackages()
+
+# create a df containing all patch coordinates, make headers same
+# and remove richness and block columns
+df_patches <- fread("PatchRichnessEnd-101.csv")
+colnames(df_patches)[1] <- "X"
+colnames(df_patches)[2] <- "Y"
+colnames(df_patches)[3] <- "patch.ID"
+df_patches <- df_patches[,-c(4:5)]
+head(df_patches)
+
+# sort the patch list
+df_patches <- df_patches[order(df_patches[["patch.ID"]]),]
+df_patches <- df_patches[,-3]
+head(df_patches)
+
+# make break points
+break_points <- seq(0, 30, by = 2)
+
+# local mantel correlogram with vegan
+mantel_vegan <- function(i) {
+    result <- mantel.correlog(species_dists[[i]],
+                              XY = df_patches,
+                              break.pts = break_points,
+                              cutoff = FALSE, nperm=100)
+    return(
+        result
+    )
+    gc()
+}
+## RUN 1
+message("start 1 ", Sys.time())
+
+# load in species_dists
+species_dists <- readRDS("~/mantel_files/16nurs_result/sp_dist_16nurs_1.rds")
+
+# change to only first 15
+species_dists <- species_dists[1]
+gc()
+
+v_result <- mclapply(1, mantel_vegan, mc.cores = 1)
+
+# save as rds
+saveRDS(v_result, "~/mantel_files/16nurs_result/local_results/local_1.rds")
+message("finished 1 ", Sys.time())
+q()
